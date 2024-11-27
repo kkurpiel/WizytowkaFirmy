87090f763d54c71b20929c47d9452870e7d193af
Konrad Kurpiel <168281997+borubar3k@users.noreply.github.com>
2024-11-27 11:09:55 +0100

    Dodaj pliki projektów.

---
+++ WizytowkaFirmy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WizytowkaFirmy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
